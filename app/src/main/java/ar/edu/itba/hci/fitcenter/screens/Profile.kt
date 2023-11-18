e70c54b703d5fc7fae8b75cc82dd071dc73d0aa3
motegui <80490650+motegui@users.noreply.github.com>
2023-11-18 11:02:55 -0300

    Icon to  profile

--- app/src/main/java/ar/edu/itba/hci/fitcenter/screens/Profile.kt
+++ app/src/main/java/ar/edu/itba/hci/fitcenter/screens/Profile.kt
@@ -2,18 +2,37 @@ package ar.edu.itba.hci.fitcenter.screens
 
 import ar.edu.itba.hci.fitcenter.api.Store
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.runtime.Composable
-import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import ar.edu.itba.hci.fitcenter.ui.theme.FitcenterTheme
 
-
 @Composable
 fun Profile(navController: NavController? = null, store: Store? = null) {
-    Text("This is the profile screen")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top, //quiero arriba
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(
+            Icons.Default.AccountCircle,
+            contentDescription = "User Icon",
+            modifier = Modifier.size(100.dp) // adjust the size as needed
+        )
+       
+    }
 }
-
 
 @Preview(name="Light Mode")
 @Preview(
