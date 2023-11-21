0b36cd36a1549b3a30d46152ed9eb77a1839c1a3
Nate <14845347+garlic-os@users.noreply.github.com>
2023-11-21 13:52:32 -0300

    More progress on Execute Routine

--- app/src/main/java/ar/edu/itba/hci/fitcenter/screens/ExecuteRoutine.kt
+++ app/src/main/java/ar/edu/itba/hci/fitcenter/screens/ExecuteRoutine.kt
@@ -2,13 +2,27 @@ package ar.edu.itba.hci.fitcenter.screens
 /*
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ar.edu.itba.hci.fitcenter.R
 import ar.edu.itba.hci.fitcenter.SampleData
 import ar.edu.itba.hci.fitcenter.api.Models
 import ar.edu.itba.hci.fitcenter.ui.theme.FitcenterTheme
@@ -34,27 +48,59 @@ class ExecuteRoutineActivity : ComponentActivity() {
 >>>>>>> Stashed changes:app/src/main/java/ar/edu/itba/hci/fitcenter/ExecuteRoutineActivity.kt
 
 @Composable
+fun Timer(seconds: Int, onExpire: () -> Unit) {
+    Text(seconds.toString())
+    Button(onClick = onExpire) {
+        Text(stringResource(R.string.pause))
+    }
+}
+
+
+@Composable
 fun Execution(routine: Models.MegaRoutine, detailed: Boolean) {
-    Column {
+    var cycleNum by remember { mutableIntStateOf(0) }
+    var repNum by remember { mutableIntStateOf(0) }
+    val currentCycle by remember { derivedStateOf { routine.megaCycles[cycleNum] } }
+    val currentRep by remember { derivedStateOf { currentCycle.cycleExercises[repNum] } }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .padding(
+                top = 36.dp,
+                start = 48.dp,
+                end   = 48.dp,
+                bottom = 84.dp
+            )
+    ) {
         // Header
         Row {
-            Text("Exit")
+            Spacer(modifier = Modifier.weight(1f))
+            Button(onClick = {}) {
+                Text(stringResource(R.string.quit))
+            }
         }
 
         // Title
-        Text("Cycle #/${routine.megaCycles.size}")
-        Text("Repetition #/#")
-        Text(routine.name)
+        Text("Cycle ${cycleNum + 1}/${routine.megaCycles.size}")
+        Text("Repetition ${repNum + 1}/${currentCycle.repetitions}")
+        Text(currentCycle.name)
+
+        Spacer(modifier = Modifier.weight(1f))
 
         // Nav
         Row {
             Text("Previous")
+            Spacer(modifier = Modifier.weight(1f))
             Text("Next")
         }
 
+        Spacer(modifier = Modifier.weight(1f))
+
         // Body
-        Text("Timer")
-        Text("Pause")
+        Timer(currentRep.duration) {
+
+        }
     }
 }
 
@@ -70,13 +116,13 @@ fun ExecutionPreview(detailed: Boolean) {
     }
 }
 
-@Preview(showBackground = true)
+@Preview(name = "Simple")
 @Composable
 fun SimpleExecutionPreview() {
     ExecutionPreview(detailed = false)
 }
 
-@Preview(showBackground = true)
+@Preview(name = "Detailed")
 @Composable
 fun DetailedExecutionPreview() {
     ExecutionPreview(detailed = true)
