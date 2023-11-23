073facdd76f28712aaf40f846fc8e4fdab51c968
Nate <14845347+garlic-os@users.noreply.github.com>
2023-11-23 14:19:03 -0300

    Fix date format
    The API uses Unix Timestamps

--- app/src/main/java/ar/edu/itba/hci/fitcenter/components/Date.kt
+++ app/src/main/java/ar/edu/itba/hci/fitcenter/components/Date.kt
@@ -1,10 +1,9 @@
 package ar.edu.itba.hci.fitcenter.components
 
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
+import java.text.DateFormat.getDateInstance
 
-fun formatDate(dateValue: Long): String {
-    val date = LocalDate.parse(dateValue.toString(), DateTimeFormatter.BASIC_ISO_DATE)
-    val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy") // You can adjust the pattern as needed
-    return date.format(formatter)
+fun formatDate(unixTimestamp: Long): String {
+    val sdf = getDateInstance()
+    val date = java.util.Date(unixTimestamp * 1000)
+    return sdf.format(date)
 }
