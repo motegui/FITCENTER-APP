81530739288fa967eb83c8eba432873993a2eaa7
Nate <14845347+garlic-os@users.noreply.github.com>
2023-11-23 19:24:51 -0300

    Extract fetching routines to component

---
+++ app/src/main/java/ar/edu/itba/hci/fitcenter/components/RoutinesListEffect.kt
@@ -0,0 +1,34 @@
+package ar.edu.itba.hci.fitcenter.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import ar.edu.itba.hci.fitcenter.api.Models
+import ar.edu.itba.hci.fitcenter.api.Store
+import kotlinx.coroutines.CancellationException
+
+@Composable
+fun RoutinesListEffect(
+    navController: NavController? = null,
+    store: Store? = null,
+    onValueChange: (List<Models.FullRoutine>?) -> Unit
+) {
+    LaunchedEffect(store) {
+        if (store == null) return@LaunchedEffect
+        onValueChange(null)
+        try {
+            onValueChange(store.fetchRoutines())
+        } catch (error: Exception) {
+            if (error is CancellationException) {
+                return@LaunchedEffect
+            }
+            store.logout()
+            navController?.navigate("login") {
+                popUpTo(navController.graph.findStartDestination().id) {
+                    saveState = true
+                }
+            }
+        }
+    }
+}
