cab1b2b5d750990eba00302de768ff183731c50c
maiwenn <maiwenn.boizumault@insa-rennes.fr>
2023-11-24 13:34:53 -0300

    Updated formatDate

--- app/src/main/java/ar/edu/itba/hci/fitcenter/components/Date.kt
+++ app/src/main/java/ar/edu/itba/hci/fitcenter/components/Date.kt
@@ -1,9 +1,18 @@
 package ar.edu.itba.hci.fitcenter.components
 
-import java.text.DateFormat.getDateInstance
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
-fun formatDate(unixTimestamp: Long): String {
-    val sdf = getDateInstance()
-    val date = java.util.Date(unixTimestamp * 1000)
-    return sdf.format(date)
+
+fun formatDate(timestamp: Long): String {
+    val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+    val date = Date(timestamp)
+    return dateFormat.format(date)
+}
+
+fun main() {
+    val timestamp = 1602646871112L
+    val formattedDate = formatDate(timestamp)
+    println("Timestamp $timestamp converted to date: $formattedDate")
 }
