ce4d90b0f0af7f4cfefe54989ba6c27e8f856199
maiwenn <maiwenn.boizumault@insa-rennes.fr>
2023-11-24 00:23:01 -0300

    remove unnecessary field of modifier

--- app/src/main/java/ar/edu/itba/hci/fitcenter/components/OrderButtons.kt
+++ app/src/main/java/ar/edu/itba/hci/fitcenter/components/OrderButtons.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
@@ -146,7 +145,7 @@ fun SortingButton(
         colors = ButtonDefaults.buttonColors(
             containerColor = if (isSelected) MaterialTheme.colorScheme.primary else Color.Gray
         ),
-        modifier = modifier.widthIn()
+        modifier = modifier
     ) {
         Text(
             text = text,
